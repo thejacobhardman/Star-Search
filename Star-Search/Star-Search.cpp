ac3f328dfd03bf2d82d0bb0c9e2d2459185f7fcf
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-11-02 15:07:22 -0600

    start writing program

--- Star-Search/Star-Search.cpp
+++ Star-Search/Star-Search.cpp
@@ -4,10 +4,64 @@
 // 11/2/19
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
+void main()
 {
-    std::cout << "Hello World!\n";
+	bool isRunning = true;
+	string userInput;
+
+	
+	while (isRunning) {
+
+		double scores[5] = {0, 0, 0, 0, 0};
+		
+		for (int i = 0; i < 5; i++) {
+			getJudgeData(scores[i]);
+		}
+
+
+
+		for (int i = 0; i < 5; i++) {
+			calcScore(scores[i]);
+		}
+
+		bool userConfirm = false;
+		while (userConfirm == false) {
+			cout << "Would you like to run the program again? (Y/N): ";
+			cin >> userInput;
+
+			if (userInput == "Y" || userInput == "y") {
+				userConfirm = true;
+			}
+			else if (userInput == "N" || userInput == "n") {
+				userConfirm = true;
+				isRunning = false;
+			}
+			else {
+				cout << "Please enter a valid selection." << endl;
+			}
+		}
+	}
+}
+
+void getJudgeData(double& score) {
+	do {
+		cout << "Enter a judge's score: ";
+		cin >> score;
+	} while (score > 10 && score < 0);
+}
+
+void calcScore(double& score) {
+	cout << "Blah";
+}
+
+int findLowest() {
+	return 0;
+}
+
+int findHighest() {
+	return 0;
 }
